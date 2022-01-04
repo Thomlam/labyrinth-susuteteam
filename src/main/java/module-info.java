d28d514fc8d49490d1ea14f0053a75b29aaa1c53
ThomLam <lamiablethomas@gmail.com>
2022-01-04 13:53:40 +0100

    First Commit Starting Projet

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.labyrinthsusuteteam {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.labyrinthsusuteteam to javafx.fxml;
+    exports com.example.labyrinthsusuteteam;
+}
